ab03fbc67e81bb0b31510afd0b2d3764a58ba539
RegistryAlive <99369527+RegistryAlive@users.noreply.github.com>
2023-08-12 03:48:35 +0800

    Add files via upload

--- WLRITranslation.ps1
+++ WLRITranslation.ps1
@@ -47,6 +47,7 @@ $dataFilesToDownload = @(
     "https://github.com/RegistryAlive/4444/raw/main/data.zip",
     "https://cdn.discordapp.com/attachments/758103145208479795/1051957594975121538/menu.zip"
 )
+
 #KEKW
 $serverIniFile = Join-Path $workingDirectory "SERVER.INI"
 $serverIniURL = "https://github.com/RegistryAlive/4444/raw/main/SERVER.INI"
